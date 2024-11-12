765d28ad76f2d9dbd441d9dcdcf5e2da89539b0e
Yigit <yigistaff@gmail.com>
2024-11-12 16:09:52 -0500

    child Meat class has been completed.

--- src/main/java/com/pluralsight/Meat.java
+++ src/main/java/com/pluralsight/Meat.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
-public class Meat {
+public class Meat extends Topping {
+    public Meat(String name) {
+        super(name);
+    }
+
+    @Override
+    public double calculatePrice(int size) {
+        if (size == 4) return 1.00;
+        else if (size == 8) return 2.00;
+        else return 3.00;
+    }
 }
