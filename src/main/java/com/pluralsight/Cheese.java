01f7c746c805747ed471a93985f4c0663f05623e
Yigit <yigistaff@gmail.com>
2024-11-12 17:40:23 -0500

    Child Cheese class has been completed.

--- src/main/java/com/pluralsight/Cheese.java
+++ src/main/java/com/pluralsight/Cheese.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
-public class Cheese {
+public class Cheese extends Topping {
+    public Cheese(String name) {
+        super(name);
+    }
+
+    @Override
+    public double calculatePrice(int size) {
+        if (size == 4) return 0.75;
+        else if (size == 8) return 1.50;
+        else return 2.25;
+    }
 }
