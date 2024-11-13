f6b00e65d7ba2d7d087d62315080154526cfd2cc
Yigit <yigistaff@gmail.com>
2024-11-13 15:23:15 -0500

    customizeSandwich method has been added to UserInterface class.

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -51,13 +51,13 @@ public class UserInterface {
                 switch (choice) {
                     case 1:
 
-                      
+                        break;
                     case 2:
 
-
+                        break;
                     case 3:
 
-
+                        break;
                     case 4:
 
                         ordering = false;
@@ -73,5 +73,61 @@ public class UserInterface {
                 System.out.println("Please enter a number.");
             }
         }
+    }
+
+    private void customizeSandwich() {
+        System.out.print("Enter sandwich size (4, 8, or 12): ");
+        String size = scanner.nextLine();
+        System.out.print("Enter bread type (white, wheat, rye, wrap): ");
+        String breadType = scanner.nextLine();
+        Sandwich newSandwich = new Sandwich(size, breadType);
+
+        boolean addingToppings = true;
+        while (addingToppings) {
+            System.out.println("\n--- Toppings ---");
+            System.out.println("1) Add Meat");
+            System.out.println("2) Add Cheese");
+            System.out.println("3) Add Regular Topping");
+            System.out.println("4) Add Sauce");
+            System.out.println("5) Finish Sandwich");
+            try {
+                int toppingChoice = Integer.parseInt(scanner.nextLine());
+                switch (toppingChoice) {
+                    case 1:
+                        System.out.print("Enter meat type: ");
+                        newSandwich.addTopping(new Meat(scanner.nextLine()));
+                        break;
+                    case 2:
+                        System.out.print("Enter cheese type: ");
+                        newSandwich.addTopping(new Cheese(scanner.nextLine()));
+                        break;
+                    case 3:
+                        System.out.print("Enter regular topping: ");
+                        newSandwich.addTopping(new RegularTopping(scanner.nextLine()));
+                        break;
+                    case 4:
+                        System.out.print("Enter sauce (or 'done' to finish): ");
+                        String sauce = scanner.nextLine();
+                        if (!sauce.equalsIgnoreCase("done")) {
+                            newSandwich.addSauce(sauce);
+                        }
+                        break;
+                    case 5:
+                        addingToppings = false;
+                        break;
+                    default:
+                        System.out.println("Invalid option. Please try again.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a number for the topping choice.");
+            }
+        }
+
+        System.out.print("Do you want the sandwich toasted? (y/n): ");
+        if (scanner.nextLine().equalsIgnoreCase("y")) {
+            newSandwich.setToasted(true);
+        }
+        currentOrder.addSandwich(newSandwich);
+        System.out.println("Sandwich added to order.");
     }
 }
