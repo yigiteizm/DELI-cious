524ca4b034dc5c1ef5399468f6f7ab5ca5782032
Yigit <yigistaff@gmail.com>
2024-11-12 20:20:03 -0500

    Child RegularTopping class has been extended to parent class Topping and completed.

--- src/main/java/com/pluralsight/RegularTopping.java
+++ src/main/java/com/pluralsight/RegularTopping.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
-public class RegularTopping {
+public class RegularTopping extends Topping {
+    public RegularTopping(String name) {
+        super(name);
+    }
+
+    @Override
+    public double calculatePrice(int size) {
+        return 0.0;
+    }
 }
