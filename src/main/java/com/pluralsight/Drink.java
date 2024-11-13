f5dcf0c46ebc0427a339fc292ec2337c7c9105e1
Yigit <yigistaff@gmail.com>
2024-11-13 10:45:27 -0500

    calculatePrice method has been added to Drink class.

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -4,5 +4,8 @@ public class Drink {
     private String size;
     private String flavors;
 
-
+    public Drink(String size, String flavors) {
+        this.size = size;
+        this.flavors = flavors;
     }
+}
