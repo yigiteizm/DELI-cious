22b5c04f084a05d1cfc206772518f66d4a94bdeb
Yigit <yigistaff@gmail.com>
2024-11-13 10:41:18 -0500

    constructor has been added to Drink class.

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -4,8 +4,5 @@ public class Drink {
     private String size;
     private String flavors;
 
-    public Drink(String size, String flavors) {
-        this.size = size;
-        this.flavors = flavors;
+
     }
-}
