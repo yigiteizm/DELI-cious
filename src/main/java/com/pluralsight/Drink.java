d87e30fa25493b033aba977ac2a5b3c404488301
Yigit <yigistaff@gmail.com>
2024-11-13 10:49:52 -0500

    Override toString method has been added to Drink class.

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -8,4 +8,19 @@ public class Drink {
         this.size = size;
         this.flavors = flavors;
     }
+
+    public double calculatePrice() {
+        if ("small".equals(size)) {
+            return 2.00;
+        } else if ("medium".equals(size)) {
+            return 2.50;
+        } else {
+            return 3.00;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Drink - Size: %s, Flavors: %s - $%.2f", size, flavors, calculatePrice());
+    }
 }
