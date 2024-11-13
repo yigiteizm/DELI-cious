ad2cb2b4dbbd58084f36128f2f52c1ee13c2275e
Yigit <yigistaff@gmail.com>
2024-11-13 10:03:57 -0500

    Chip class has been modified.

--- src/main/java/com/pluralsight/Chip.java
+++ src/main/java/com/pluralsight/Chip.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
 public class Chip {
+    private String type;
+
+    public Chip(String type) {
+        this.type = type;
+    }
+
+    public double calculatePrice() {
+        return 1.50;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Chip - Type: %s - $%.2f", type, calculatePrice());
+    }
 }
