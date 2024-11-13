f5e9d2828c965b95b7e98d5e50400e9ed9686b9b
Yigit <yigistaff@gmail.com>
2024-11-13 15:50:06 -0500

    Main method has been completed.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,8 @@
 package com.pluralsight;
 
 public class Main {
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.displayMainMenu();
+    }
 }
