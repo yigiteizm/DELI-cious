dbb22ef5dfc039dfcc3784b82829955268a0423b
Yigit <yigistaff@gmail.com>
2024-11-13 15:19:07 -0500

    createNewOrder method has been added to UserInterface class.

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -22,7 +22,7 @@ public class UserInterface {
                 int choice = Integer.parseInt(scanner.nextLine());
                 switch (choice) {
                     case 1:
-
+                        createNewOrder();
                         break;
                     case 0:
                         running = false;
@@ -30,7 +30,46 @@ public class UserInterface {
                     default:
                         System.out.println("Invalid option. Please try again.");
                 }
-            } catch (Exception e) {
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a number.");
+            }
+        }
+    }
+
+    private void createNewOrder() {
+        currentOrder = new Order();
+        boolean ordering = true;
+        while (ordering) {
+            System.out.println("\n--- Order Menu ---");
+            System.out.println("1) Add Sandwich");
+            System.out.println("2) Add Drink");
+            System.out.println("3) Add Chips");
+            System.out.println("4) Checkout");
+            System.out.println("0) Cancel Order");
+            try {
+                int choice = Integer.parseInt(scanner.nextLine());
+                switch (choice) {
+                    case 1:
+
+                      
+                    case 2:
+
+
+                    case 3:
+
+
+                    case 4:
+
+                        ordering = false;
+                        break;
+                    case 0:
+                        System.out.println("Order canceled.");
+                        ordering = false;
+                        break;
+                    default:
+                        System.out.println("Invalid option. Please try again.");
+                }
+            } catch (NumberFormatException e) {
                 System.out.println("Please enter a number.");
             }
         }
