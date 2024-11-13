bf8bf52f83b90a58c0f28415172e818820514c30
Yigit <yigistaff@gmail.com>
2024-11-13 10:37:42 -0500

    Size and flavors attributes have been added to Drink class.

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
 public class Drink {
+    private String size;
+    private String flavors;
+
+    public Drink(String size, String flavors) {
+        this.size = size;
+        this.flavors = flavors;
+    }
 }
