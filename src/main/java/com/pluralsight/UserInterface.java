8197762547518686fb0b19202eadb0a8f059a1cc
Yigit <yigistaff@gmail.com>
2024-11-13 14:33:58 -0500

    Attributes have been added to UserInterface class.

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
+    private Scanner scanner;
+    private Order currentOrder;
+    private OrderFileManager fileManager;
+
 }
