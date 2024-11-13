9295ca5ed03a7e4b998b68e399165d446a051865
Yigit <yigistaff@gmail.com>
2024-11-13 08:48:17 -0500

    Child Sauce class has been extended to parent class Topping and completed.

--- src/main/java/com/pluralsight/Sauce.java
+++ src/main/java/com/pluralsight/Sauce.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
-public class Sauce {
+public class Sauce extends Topping {
+    public Sauce(String name) {
+        super(name);
+    }
+
+    @Override
+    public double calculatePrice(int size) {
+        return 0.0;
+    }
 }
