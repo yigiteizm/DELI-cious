2d9db5cd949f12b84ded95803796a40f7bccfc1e
Yigit <yigistaff@gmail.com>
2024-11-13 11:25:19 -0500

    LocalDateTime and DateTimeFormatter have been added to OrderFileManager class.

--- src/main/java/com/pluralsight/OrderFileManager.java
+++ src/main/java/com/pluralsight/OrderFileManager.java
@@ -1,7 +1,11 @@
 package com.pluralsight;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class OrderFileManager {
     public void saveReceipt(Order order) {
-       
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        String fileName = "receipts/" + LocalDateTime.now().format(formatter) + ".txt";
     }
 }
