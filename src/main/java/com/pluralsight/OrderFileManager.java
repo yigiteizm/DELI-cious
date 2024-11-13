c11f33eeb704da21674f12bd84954ec2b7244afe
Yigit <yigistaff@gmail.com>
2024-11-13 11:28:47 -0500

    File writer and try catch block has been added to OrderFileManager class.

--- src/main/java/com/pluralsight/OrderFileManager.java
+++ src/main/java/com/pluralsight/OrderFileManager.java
@@ -1,5 +1,9 @@
 package com.pluralsight;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -7,5 +11,15 @@ public class OrderFileManager {
     public void saveReceipt(Order order) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
         String fileName = "receipts/" + LocalDateTime.now().format(formatter) + ".txt";
+
+        try {
+            Files.createDirectories(Paths.get("receipts"));
+            FileWriter writer = new FileWriter(fileName);
+            writer.write(order.toString());
+            writer.close();
+            System.out.println("Order saved to " + fileName);
+        } catch (Exception e) {
+            System.out.println("Error saving order: " + e.getMessage());
+        }
     }
 }
