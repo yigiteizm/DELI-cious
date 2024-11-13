f0287cc08067ab288772d0dce475f53c58cd99f2
Yigit <yigistaff@gmail.com>
2024-11-13 15:16:48 -0500

    displaymainMenu method has been added for display Home Screen to user.

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -11,4 +11,28 @@ public class UserInterface {
         this.scanner = new Scanner(System.in);
         this.fileManager = new OrderFileManager();
     }
+
+    public void displayMainMenu() {
+        boolean running = true;
+        while (running) {
+            System.out.println("\n--- Main Menu ---");
+            System.out.println("1) New Order");
+            System.out.println("0) Exit");
+            try {
+                int choice = Integer.parseInt(scanner.nextLine());
+                switch (choice) {
+                    case 1:
+
+                        break;
+                    case 0:
+                        running = false;
+                        break;
+                    default:
+                        System.out.println("Invalid option. Please try again.");
+                }
+            } catch (Exception e) {
+                System.out.println("Please enter a number.");
+            }
+        }
+    }
 }
